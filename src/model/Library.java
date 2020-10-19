c91311367001536a51129d52dfc1889542dc931f
lehuuphuc2349 <57623623+lehuuphuc2349@users.noreply.github.com>
2020-10-19 09:27:02 +0700

    commit

--- src/model/Library.java
+++ src/model/Library.java
@@ -32,6 +32,78 @@ public class Library {
 	}
 	
 //	Getter Setter
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Librian getLibrian() {
+		return librian;
+	}
+
+	public void setLibrian(Librian librian) {
+		this.librian = librian;
+	}
+
+	public ArrayList<Person> getPersons() {
+		return persons;
+	}
+
+	public void setPersons(ArrayList<Person> persons) {
+		this.persons = persons;
+	}
+
+	public ArrayList<Book> getBooksInLibrary() {
+		return booksInLibrary;
+	}
+
+	public void setBooksInLibrary(ArrayList<Book> booksInLibrary) {
+		this.booksInLibrary = booksInLibrary;
+	}
+
+	public ArrayList<Loan> getLoans() {
+		return loans;
+	}
+
+	public void setLoans(ArrayList<Loan> loans) {
+		this.loans = loans;
+	}
+
+	public int getBookReturnDeadLine() {
+		return bookReturnDeadLine;
+	}
+
+	public void setBookReturnDeadLine(int bookReturnDeadLine) {
+		this.bookReturnDeadLine = bookReturnDeadLine;
+	}
+
+	public double getPerDayFine() {
+		return perDayFine;
+	}
+
+	public void setPerDayFine(double perDayFine) {
+		this.perDayFine = perDayFine;
+	}
+
+	public int getHoldRequestExpiry() {
+		return HoldRequestExpiry;
+	}
+
+	public void setHoldRequestExpiry(int HoldRequestExpiry) {
+		this.HoldRequestExpiry = HoldRequestExpiry;
+	}
+
+	public static Library getObj() {
+		return obj;
+	}
+
+	public static void setObj(Library obj) {
+		Library.obj = obj;
+	}
 	
 	
 	
